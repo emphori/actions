c8bbdb88a706a60c2914b3597711f6ad1c18c095
Iain Reid <iainreid382@gmail.com>
2020-12-30 18:59:38 +0000

    fix: Navigate to directory after clone

--- git-mirror-repository/git-mirror-repository.sh
+++ git-mirror-repository/git-mirror-repository.sh
@@ -5,5 +5,6 @@ set -x
 GIT_MIRROR_SOURCE_URL=$1
 GIT_MIRROR_REMOTE_URL=$2
 
-git clone --mirror ${GIT_MIRROR_SOURCE_URL}
+git clone --mirror ${GIT_MIRROR_SOURCE_URL} target_dir
+cd target_dir
 git push --mirror ${GIT_MIRROR_REMOTE_URL}
