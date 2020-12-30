745ab9767ad0d91d741dd491ee07307619ab11c5
Iain Reid <iainreid382@gmail.com>
2020-12-30 18:55:21 +0000

    fix: Correct mirroring technique

--- git-mirror-repository/git-mirror-repository.sh
+++ git-mirror-repository/git-mirror-repository.sh
@@ -2,9 +2,8 @@
 
 set -x
 
-GIT_MIRROR_REMOTE_URL=$1
+GIT_MIRROR_SOURCE_URL=$1
+GIT_MIRROR_REMOTE_URL=$2
 
-# Strip out GitHub specific refs
-git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
-
+git clone --mirror ${GIT_MIRROR_SOURCE_URL}
 git push --mirror ${GIT_MIRROR_REMOTE_URL}
