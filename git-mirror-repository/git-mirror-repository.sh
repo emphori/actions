89b5e3e95fd9425e6fff8bfa685e793037dffb63
Iain Reid <iainreid382@gmail.com>
2020-12-30 19:05:07 +0000

    feat: Add better error handling when cloning

--- git-mirror-repository/git-mirror-repository.sh
+++ git-mirror-repository/git-mirror-repository.sh
@@ -6,5 +6,16 @@ GIT_MIRROR_SOURCE_URL=$1
 GIT_MIRROR_REMOTE_URL=$2
 
 git clone --mirror ${GIT_MIRROR_SOURCE_URL} target_dir
+
+if [ $? -ne 0 ]; then
+  echo "Failed to checkout source repository"
+  exit 1
+fi
+
 cd target_dir
 git push --mirror ${GIT_MIRROR_REMOTE_URL}
+
+if [ $? -ne 0 ]; then
+  echo "Failed to push mirror repository"
+  exit 1
+fi
