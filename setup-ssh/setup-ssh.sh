ec1be71aa221837e37f7c1fc591f1b1a926efc54
Iain Reid <iainreid382@gmail.com>
2020-12-30 17:50:18 +0000

    fix: Set correct permissions on id_rsa file

--- setup-ssh/setup-ssh.sh
+++ setup-ssh/setup-ssh.sh
@@ -7,8 +7,8 @@ SSH_KNOWN_HOSTS=$2
 
 mkdir -p ~/.ssh
 
-# Write the private key to disk
-echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+# Write the private key to disk with correct permissions
+install -m 600 <(echo "$SSH_PRIVATE_KEY") ~/.ssh/id_rsa
 
 # If supplied, setup the known hosts
 if [ ! -z "$SSH_KNOWN_HOSTS" ]; then
