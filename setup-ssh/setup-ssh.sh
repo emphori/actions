0102ffaea4cd3ab4a71610e7d709a44e98252fda
Iain Reid <iainreid382@gmail.com>
2020-12-30 17:45:36 +0000

    fix: Remove usage of ssh-agent

--- setup-ssh/setup-ssh.sh
+++ setup-ssh/setup-ssh.sh
@@ -7,11 +7,8 @@ SSH_KNOWN_HOSTS=$2
 
 mkdir -p ~/.ssh
 
-# Start the SSH agent and expose SSH_AUTH_SOCK and SSH_AGENT_PID
-eval `ssh-agent -s`
-
-# Add the key to the SSH agent
-ssh-add - <<< "$SSH_PRIVATE_KEY"
+# Write the private key to disk
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 
 # If supplied, setup the known hosts
 if [ ! -z "$SSH_KNOWN_HOSTS" ]; then
