6596b727e5f9974c797f34e673ef3d1631d9aadd
Iain Reid <iainreid382@gmail.com>
2020-12-30 18:30:00 +0000

    feat: Remove GitHub specific refs

--- git-mirror-repository/git-mirror-repository.sh
+++ git-mirror-repository/git-mirror-repository.sh
@@ -4,4 +4,7 @@ set -x
 
 GIT_MIRROR_REMOTE_URL=$1
 
+# Strip out GitHub specific refs
+git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
+
 git push --mirror ${GIT_MIRROR_REMOTE_URL}
