3b61c1c65940be5d73d0a749a345855a6dcd60e9
Iain Reid <iainreid382@gmail.com>
2021-01-13 23:21:55 +0000

    feat(#4): Switch scripts to use plain Bourne shell

--- git-mirror-repository/git-mirror-repository.sh
+++ git-mirror-repository/git-mirror-repository.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
-
-set -x
+#!/bin/sh -e
 
 GIT_MIRROR_SOURCE_URL=$1
 GIT_MIRROR_REMOTE_URL=$2
 
-git clone --mirror ${GIT_MIRROR_SOURCE_URL} target_dir
-
-if [ $? -ne 0 ]; then
+if ! git clone --mirror "$GIT_MIRROR_SOURCE_URL" target_dir; then
   echo "Failed to checkout source repository"
   exit 1
 fi
 
-cd target_dir
-git push --mirror ${GIT_MIRROR_REMOTE_URL}
+cd target_dir || {
+  echo "Failed to enter cloned repository"
+  exit 1
+}
 
-if [ $? -ne 0 ]; then
+if ! git push --mirror "$GIT_MIRROR_REMOTE_URL"; then
   echo "Failed to push mirror repository"
   exit 1
 fi
--- setup-ssh/setup-ssh.sh
+++ setup-ssh/setup-ssh.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-set -x
+#!/bin/sh -e
 
 SSH_PRIVATE_KEY=$1
 SSH_KNOWN_HOSTS=$2
@@ -8,9 +6,10 @@ SSH_KNOWN_HOSTS=$2
 mkdir -p ~/.ssh
 
 # Write the private key to disk with correct permissions
-install -m 600 <(echo "$SSH_PRIVATE_KEY") ~/.ssh/id_rsa
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 
 # If supplied, setup the known hosts
-if [ ! -z "$SSH_KNOWN_HOSTS" ]; then
+if [ -n "$SSH_KNOWN_HOSTS" ]; then
   echo "$SSH_KNOWN_HOSTS" >> ~/.ssh/known_hosts
 fi
