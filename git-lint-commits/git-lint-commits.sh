645e02896c4403e5f0752d40239693cabebaf5e2
Iain Reid <iainreid382@gmail.com>
2021-01-28 23:52:09 +0000

    fix(#7): Automatically assume origin for target

--- git-lint-commits/git-lint-commits.sh
+++ git-lint-commits/git-lint-commits.sh
@@ -4,10 +4,13 @@ GIT_COMMIT_GREP=${1:-"^(fix|feat|chore)(\(#[0-9]*\))?\!?: .*"}
 GIT_TARGET_BRANCH=${2:-"master"}
 GIT_SOURCE_BRANCH=${2:-"HEAD"}
 
-revision="$GIT_TARGET_BRANCH..$GIT_SOURCE_BRANCH"
+revision="origin/$GIT_TARGET_BRANCH..$GIT_SOURCE_BRANCH"
 
 # Log the number of incoming commits
-printf "%s new commit(s)\n" "$(git rev-list --count "$revision")"
+printf "%s new commit(s)\n" "$(git rev-list --count "$revision")" || {
+  echo "Unable to count incoming commits, is the target branch or revision available?"
+  exit 1
+}
 
 # Validate commit subjects
 commits=$(git log --no-merges --grep="$GIT_COMMIT_GREP" -E --invert-grep "$revision") || {
